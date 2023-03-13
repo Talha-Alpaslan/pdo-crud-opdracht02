e31bc940adc1887c9eae003c754d3be50608a96b
Talha-Alpaslan <337247@student.mboutrecht.nl>
2023-03-13 09:37:22 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pizza</title>
+</head>
+<body>
+    <h1>pizza</h1>
+
+    <form>
+        <label>bodemformaat</label>
+        <select>
+            <option>20</option>
+            <option>25</option>
+            <option>30</option>
+            <option>35</option>
+            <option>40</option>
+        </select><br>
+        <label>saus</label>
+        <select>
+            <option>tomatensaus</option>
+            <option>extra tomatensaus</option>
+            <option>spicy tomatensaus</option>
+            <option>BBQ saus</option>
+            <option>Creme fraiche</option>
+        </select><br>
+        <label>pizzatoppings</label><br>
+        <input type="radio">
+            vegan
+        </input><br>
+        <input type="radio">
+            kaas
+        </input><br>
+        <input type="radio">
+            vlees
+        </input><br>
+        <input type="radio">
+            paprika
+        </input><br>
+        <input type="radio">
+            champignon
+        </input><br>
+        <label>kruiden</label><br>
+        <input type="checkbox">
+            rode peper
+        </input><br>
+        <input type="checkbox">
+            zwarte peper
+        </input><br>
+        <input type="checkbox">
+            oregano
+        </input><br>
+        <button type="submit">verstuur</button><br>
+    </form>
+</body>
+</html>
+
+<?php
+
+?>
